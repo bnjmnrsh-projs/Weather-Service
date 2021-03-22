063859fe0d41e9033dec6113e9eb31ca2f8a9ac8
bnjmnrsh <bnjmnrsh@gmail.com>
2021-03-22 19:42:21 +0000

    Comments and caching & Documentation

--- middlemanAPI/worker.js
+++ middlemanAPI/worker.js
@@ -1,13 +1,13 @@
 /**
  * Cloudflare Worker middleman API
- * ex: https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262
+ * ex: https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=28.385233&lon=-81.563873
  *
  * Environmental variable: WB_KEY
  *
  */
 
 /* Run in console on one of your 'aAllowed origin' domains to test
-fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
+fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=28.385233&lon=-81.563873')
     .then(function (response) {
         if (response.ok) {
             return response.json()
@@ -24,11 +24,25 @@ fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
  */
 
 {
-    // set to true when testing with Wrangler CLI or Workers Quick edit
-    const bDEV = false
+    // Debugging: set to true to disable origin whitelist checks
+    const bDBG = false
 
+    // Cache: The length of time for Cloudflare to perserve a cached value (Time To Live)
+    const nTTL = 1800 // (seconds), 30 min
+    // 25 min for Exirey headers
+    const nCacheCont = new Date(new Date().getTime() + 25 * 60000)
+
+    // A named array of endpoints to fetch from
+    // prettier-ignore
     const aToFetch = [
-        ['CURRENT', `https://api.weatherbit.io/v2.0/current?key=${WB_KEY}&`],
+        [
+            'USEAGE',
+            `https://api.weatherbit.io/v2.0/subscription/usage?key=${WB_KEY}&`,
+        ],
+        [
+            'CURRENT',
+            `https://api.weatherbit.io/v2.0/current?key=${WB_KEY}&`
+        ],
         [
             'HOURLY',
             `https://api.weatherbit.io/v2.0/forecast/hourly?key=${WB_KEY}&hours=48&`,
@@ -37,11 +51,14 @@ fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
             'DAILY',
             `https://api.weatherbit.io/v2.0/forecast/daily?key=${WB_KEY}&days=16&`,
         ],
-        ['ALERTS', `https://api.weatherbit.io/v2.0/alerts?key=${WB_KEY}&`],
+        [
+            'ALERTS',
+            `https://api.weatherbit.io/v2.0/alerts?key=${WB_KEY}&`
+        ],
     ]
 
     // Allowed origins whitelist
-    const aAllowed = ['https://bnjmnrsh-projs.github.io']
+    const aAllowed = ['https://YOURPROJECT.github.io']
 
     // Response headers
     const oInit = {
@@ -49,8 +66,11 @@ fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET',
             'Access-Control-Allow-Headers': '*',
+            'Cache-Control': 'public',
+            Expires: `${nCacheCont}`,
             'content-type': 'application/json;charset=UTF-8',
         },
+        cf: { cacheTtl: `${nTTL}`, cacheEverything: true },
     }
 
     /**
@@ -62,17 +82,20 @@ fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
     async function fGatherResponse(response) {
         const { headers } = response
         const sContentType = headers.get('content-type') || ''
+        const code = await response.status
+        const text = await response.statusText
+        const URL = await response.url.split('?')[0]
 
+        // We're checking for JSON header as our api will return usefull details for , 2**, 4**
         if (sContentType.includes('application/json')) {
             return JSON.stringify(await response.json())
         } else {
-            const code = await response.status
-            const text = await response.statusText
-            const URL = await response.url.split('?')[0]
-
-            return JSON.stringify({
-                error: `HTTP status: ${code} ${text}: URL: ${URL}`,
-            })
+            return JSON.stringify(
+                {
+                    error: `HTTP status: ${code} ${text}: URL: ${URL}`,
+                },
+                oInit
+            )
         }
     }
 
@@ -84,21 +107,28 @@ fetch('https://YOURWORKER.YOURACCOUNT.workers.dev/?lat=55.139856&lon=-3.704262')
      */
     async function fHandleRequest(event) {
         const oRequest = event.request
-        const oHeaders = new Headers(oInit.headers)
-        const { searchParams } = new URL(oRequest.url)
 
         // If origin domain is not whitelisted, return 403
-        if (bDEV === false) {
+        if (bDBG === false) {
             if (!aAllowed.includes(oRequest.headers.get('origin'))) {
                 return new Response(
                     'Requests are not allowed from this domain.',
                     {
                         status: 403.503,
-                        statusText: 'Not a whitelisted domian.',
+                        statusText: 'Not a whitelisted domain.',
                     }
                 )
             }
         }
+
+        const oHeaders = new Headers(oInit.headers)
+        const { searchParams } = new URL(oRequest.url)
+
+        // Capture and process any custom flags
+        // const foo = searchParams.get('foo')
+        // if (foo){
+        //     console.log(foo)
+        // }
 
         // Fetch from all the APIs
         const aResponses = await Promise.all(
