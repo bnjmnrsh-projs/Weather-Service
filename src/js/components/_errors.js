5af1ca833f710bf5b11508fe0a1022490d334ca1
bnjmnrsh <bnjmnrsh@gmail.com>
2021-07-09 16:56:30 +0100

    SCSS refactor separating component styles into own imports

--- src/js/components/_errors.js
+++ src/js/components/_errors.js
@@ -5,23 +5,21 @@
  * @returns
  */
 export const fRenderErrors = function (err) {
-    return `<div id="hud">
-                <div id="ohnos">
-                    <h3><span aria-hidden="true">⥀.⥀<br /></span>Oh Nooos!</h3>
-                    <p class="sr-only">There has been a crittical error:</p>
-                        <div>
-                            ${err.stack ? '<pre>' + err.stack + '<pre>' : ''}
-                            ${
-                                err.status
-                                    ? '<pre>' +
-                                      err.statusText +
-                                      ': ' +
-                                      err.status +
-                                      '<pre>'
-                                    : ''
-                            }
-                        </div>
-                    <img alt="" src="./svg/icons/weather/svg/wi-alien.svg"/>
-                </div>
+    return `<div id="ohnos" class="screen errors">
+                <h3><span aria-hidden="true">⥀.⥀</span><br>Oh Nooos!</h3>
+                <p class="sr-only">There has been a crittical error:</p>
+                    <div class="errors-wrap">
+                        ${err.stack ? '<code>' + err.stack + '</code>' : ''}
+                        ${
+                            err.status
+                                ? '<code>' +
+                                  err.statusText +
+                                  ': ' +
+                                  err.status +
+                                  '</code>'
+                                : ''
+                        }
+                    </div>
+                <img alt="" class="screen-icon errors-icon" src="./svg/icons/weather/svg/wi-alien.svg"/>
             </div>`
 }
