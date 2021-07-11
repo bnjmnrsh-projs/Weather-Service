e4ce363f95e4128148f9adf43fc91f2505518661
bnjmnrsh <bnjmnrsh@gmail.com>
2021-07-11 23:29:32 +0100

    Added/updated StyleLint & StyleLint Standard and Prettier configs; also EsLint Config Prettier.

--- sass.js
+++ sass.js
@@ -1,10 +1,10 @@
-var sass = require('sass')
-var fs = require('fs')
-var pkg = require('./package.json')
+const sass = require('sass')
+const fs = require('fs')
+const pkg = require('./package.json')
 
 // Configs
-var configs = {
-    name: 'BuildToolsCookbook',
+const configs = {
+    name: `${pkg.name}`,
     files: ['main.scss'],
     pathIn: 'src/scss',
     pathOut: 'docs/css',
@@ -15,13 +15,13 @@ var configs = {
 }
 
 // Banner
-var banner = `/*! ${configs.name ? configs.name : pkg.name} v${
+const banner = `/*! ${configs.name ? configs.name : pkg.name} v${
     pkg.version
 } | (c) ${new Date().getFullYear()} ${pkg.author.name} | ${
     pkg.license
 } License | ${pkg.repository.url} */`
 
-var getOptions = function (file, filename, minify) {
+const getOptions = function (file, filename, minify) {
     return {
         file: `${configs.pathIn}/${file}`,
         outFile: `${configs.pathOut}/${filename}`,
@@ -33,7 +33,7 @@ var getOptions = function (file, filename, minify) {
     }
 }
 
-var writeFile = function (pathOut, fileName, fileData, printBanner = true) {
+const writeFile = function (pathOut, fileName, fileData, printBanner = true) {
     // Create the directory path
     fs.mkdir(pathOut, { recursive: true }, function (err) {
         // If there's an error, throw it
@@ -43,9 +43,9 @@ var writeFile = function (pathOut, fileName, fileData, printBanner = true) {
         fs.writeFile(`${pathOut}/${fileName}`, fileData, function (err) {
             if (err) throw err
 
-            var data = fs.readFileSync(`${pathOut}/${fileName}`)
-            var fd = fs.openSync(`${pathOut}/${fileName}`, 'w+')
-            var insert = printBanner ? new Buffer.from(banner + '\n') : ''
+            const data = fs.readFileSync(`${pathOut}/${fileName}`)
+            const fd = fs.openSync(`${pathOut}/${fileName}`, 'w+')
+            const insert = printBanner ? new Buffer.from(banner + '\n') : ''
             fs.writeSync(fd, insert, 0, insert.length, 0)
             fs.writeSync(fd, data, 0, data.length, insert.length)
             fs.close(fd, function (err) {
@@ -56,8 +56,8 @@ var writeFile = function (pathOut, fileName, fileData, printBanner = true) {
     })
 }
 
-var parseSass = function (file, minify) {
-    var filename = `${file.slice(0, file.length - 5)}${
+const parseSass = function (file, minify) {
+    const filename = `${file.slice(0, file.length - 5)}${
         minify ? '.min' : ''
     }.css`
     sass.render(getOptions(file, filename, minify), function (err, result) {
