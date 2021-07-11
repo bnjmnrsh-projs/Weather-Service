572b5b5ff469adb9c8bbdd483b4ba9197721f8ca
bnjmnrsh <bnjmnrsh@gmail.com>
2021-07-12 00:31:49 +0100

    Addition of stylelint-scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
+    plugins: ['stylelint-scss'],
     extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+    rules: {
+        // recommended rules
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true,
+    },
 }
