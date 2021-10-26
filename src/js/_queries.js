09e9dd4ef97d799bba110f2f0fb25ad763495fbc
bnjmnrsh <bnjmnrsh@gmail.com>
2021-10-26 12:44:22 +0100

    Move query methods out of app.js

--- src/js/_queries.js
+++ src/js/_queries.js
@@ -1,5 +1,32 @@
 import { fClean } from './_helpers'
 import { RequestWithRetry } from './_request'
+
+/**
+ * Browser based location API
+ *
+ * @returns {object} coordiantes object
+ */
+const fGeoLocApi = async function () {
+  const oOptions = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+  }
+
+  const pResp = new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(
+      function (pResp) {
+        resolve(pResp.coords)
+      },
+      function (pResp) {
+        reject(pResp)
+      },
+      oOptions
+    )
+  })
+  return await pResp
+}
+
 /**
  * IP address based location API
  *
@@ -17,6 +44,35 @@ export const fIPapi = async function (sIpapiLocationApi, _oSettings) {
       throw pResp
     }
   })
+}
+
+/**
+ * Gets the user location first using locationAPI, or if the user rejects, fall back to IP address lookup.
+ *
+ * @param {string} [section='home']
+ */
+export const fGetLocation = async function (sIpapiLocationApi, _oSettings) {
+  if (navigator.geolocation) {
+    try {
+      if (_oSettings.debug) {
+        console.log('fGetLocation: Checking geoLoccation API: fGeoLocApi.')
+      }
+      return await fGeoLocApi()
+    } catch (oError) {
+      if (_oSettings.debug) {
+        console.warn(`fGetLocation: failed using fGeoLocApi: ${oError}`)
+        console.warn('Falling back to IP address lookup instead.')
+      }
+      try {
+        return await fIPapi(sIpapiLocationApi, _oSettings)
+      } catch (oError) {
+        if (_oSettings.debug) {
+          console.warn(`fGetLocation: failed sIpapiLocationApi: ${oError}`)
+        }
+        throw new Error({ ...oError })
+      }
+    }
+  }
 }
 
 /**
@@ -54,61 +110,6 @@ export const fAssembleWeatherQuery = function (urlBase, oLoc, _oSettings) {
   }
 
   return fClean(sApiQuery)
-}
-
-/**
- * Browser based location API
- *
- * @returns {object} coordiantes object
- */
-const fGeoLocApi = async function () {
-  const oOptions = {
-    enableHighAccuracy: false,
-    timeout: 5000,
-    maximumAge: 0
-  }
-
-  const pResp = new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(
-      function (pResp) {
-        resolve(pResp.coords)
-      },
-      function (pResp) {
-        reject(pResp)
-      },
-      oOptions
-    )
-  })
-  return await pResp
-}
-
-/**
- * Gets the user location
- *
- * @param {string} [section='home']
- */
-export const fGetLocation = async function (sIpapiLocationApi, _oSettings) {
-  if (navigator.geolocation) {
-    try {
-      if (_oSettings.debug) {
-        console.log('fGetLocation: Checking geoLoccation API: fGeoLocApi.')
-      }
-      return await fGeoLocApi()
-    } catch (oError) {
-      if (_oSettings.debug) {
-        console.warn(`fGetLocation: failed using fGeoLocApi: ${oError}`)
-        console.warn('Falling back to IP address lookup instead.')
-      }
-      try {
-        return await fIPapi(sIpapiLocationApi, _oSettings)
-      } catch (oError) {
-        if (_oSettings.debug) {
-          console.warn(`fGetLocation: failed sIpapiLocationApi: ${oError}`)
-        }
-        throw new Error({ ...oError })
-      }
-    }
-  }
 }
 
 /**
